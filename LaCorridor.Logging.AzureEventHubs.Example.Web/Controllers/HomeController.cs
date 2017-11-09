44652b3a6daaede7ed92c48874582029b355d469
LaCorridor <laCorridor@outlook.com>
2017-11-08 18:35:49 -0800

    Add example usage of the logger in web

--- LaCorridor.Logging.AzureEventHubs.Example.Web/Controllers/HomeController.cs
+++ LaCorridor.Logging.AzureEventHubs.Example.Web/Controllers/HomeController.cs
@@ -1,22 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
 using LaCorridor.Logging.AzureEventHubs.Example.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace LaCorridor.Logging.AzureEventHubs.Example.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            _logger.LogDebug("Entering Index.");
             return View();
         }
 
         public IActionResult About()
         {
+            _logger.LogDebug("Entering About.");
+
             ViewData["Message"] = "Your application description page.";
 
             return View();
@@ -24,6 +30,8 @@ namespace LaCorridor.Logging.AzureEventHubs.Example.Web.Controllers
 
         public IActionResult Contact()
         {
+            _logger.LogDebug("Entering Contact.");
+
             ViewData["Message"] = "Your contact page.";
 
             return View();
@@ -31,6 +39,8 @@ namespace LaCorridor.Logging.AzureEventHubs.Example.Web.Controllers
 
         public IActionResult Error()
         {
+            _logger.LogError("There is an error.");
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
--- LaCorridor.Logging.AzureEventHubs.Example.Web/Program.cs
+++ LaCorridor.Logging.AzureEventHubs.Example.Web/Program.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace LaCorridor.Logging.AzureEventHubs.Example.Web
@@ -19,6 +13,12 @@ namespace LaCorridor.Logging.AzureEventHubs.Example.Web
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(builder =>
+                {
+                    builder.AddEventHub(
+                        "Endpoint=sb://saars-sb-eh-test02-ns.servicebus.windows.net/;SharedAccessKeyName=Send;SharedAccessKey=DvQHUtZPFuBh02GmBbmq29hh9Cce452XX8BazuhTPsQ=;EntityPath=saars-sb-eh-test02",
+                        LogLevel.Debug);
+                })
                 .UseStartup<Startup>()
                 .Build();
     }
