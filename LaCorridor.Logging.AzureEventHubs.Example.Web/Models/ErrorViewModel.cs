9b7f80ef89c3f6f00fb70d1c24d6af2de46ac113
LaCorridor <laCorridor@outlook.com>
2017-11-08 18:21:33 -0800

    Add a .net core web example

---
+++ LaCorridor.Logging.AzureEventHubs.Example.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LaCorridor.Logging.AzureEventHubs.Example.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
